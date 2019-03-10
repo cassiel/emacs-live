a065c1364019bfd483758c6f7dff6570a17f09de
Nick Rothwell <nick@lynley.lan>
2019-03-10 17:59:13 +0000

    neotree.

--- etc/custom/custom-configuration.el
+++ etc/custom/custom-configuration.el
@@ -10,7 +10,9 @@
  '(org-agenda-files
    (quote
     ("~/Library/Mobile Documents/M6HJR9W95L~com~textasticapp~textastic/Documents/organisation/aims-work-2019.org" "~/Library/Mobile Documents/M6HJR9W95L~com~textasticapp~textastic/Documents/organisation/aims-home-2019.org" "~/Library/Mobile Documents/M6HJR9W95L~com~textasticapp~textastic/Documents/organisation/aims-2018.org" "~/TEXTASTIC/organisation/volatile.org" "~/TEXTASTIC/organisation/teaching.org" "~/TEXTASTIC/organisation/projects.org" "~/TEXTASTIC/organisation/german.org" "~/TEXTASTIC/organisation/admin.org" "~/TEXTASTIC/organisation/activities.org" "~/TEXTASTIC/organisation/23admiral.org")))
- '(package-selected-packages (quote (toc-org projectile coffee-mode adaptive-wrap)))
+ '(package-selected-packages
+   (quote
+    (neotree toc-org projectile coffee-mode adaptive-wrap)))
  '(sclang-auto-scroll-post-buffer t)
  '(sclang-eval-line-forward nil))
 (custom-set-faces
